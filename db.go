8dc8ee2278eaff4a687ffc2066abacf14159bd2d
helei <helei@itestor.com>
2024-03-07 11:08:06 +0800

    ReqError 调整

--- db.go
+++ db.go
@@ -82,7 +82,7 @@ func FilterWhereString(r *http.Request, query string, field string, like bool) f
 }
 
 // FilterWhereStruct 通过结构体 自动映射查询字段
-func FilterWhereStruct(s any, r *http.Request, likes ...map[string]string) func(db *gorm.DB) *gorm.DB {
+func FilterWhereStruct(s any, alias string, enableDefault bool, r *http.Request, likes ...map[string]string) func(db *gorm.DB) *gorm.DB {
 	return func(db *gorm.DB) *gorm.DB {
 		t := reflect.TypeOf(s)
 		if t.Kind() != reflect.Struct {
@@ -99,14 +99,22 @@ func FilterWhereStruct(s any, r *http.Request, likes ...map[string]string) func(
 			}
 			val := query.Get(strings.Split(tagName, ",")[0])
 			if val == "" {
+				if !enableDefault {
+					continue
+				}
 				// 如果没有传值，判断是否有默认值
 				val = t.Field(i).Tag.Get("default")
 				if val == "" {
 					continue
 				}
 			}
+
+			tableName := alias
+			if tableName == "" {
+				tableName = SnakeString(t.Name())
+			}
 			// 这里还需要解析出字段本身的名字，去数据库进行查询，通过将结构体转成蛇形方式。
-			fieldName := SnakeString(t.Field(i).Name)
+			fieldName := tableName + "." + SnakeString(t.Field(i).Name)
 			if t.Field(i).Type.String() == "int" {
 				db.Where(fieldName+"=?", val)
 			} else {
