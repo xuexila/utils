dc8582328d129181cedb23e0743714c06e2c8206
helei <helei@itestor.com>
2024-07-24 16:44:50 +0800

    AutoCreateTableWithStruct 优化
    Signed-off-by: helei <helei@itestor.com>

--- dataType/date.go
+++ dataType/date.go
@@ -3,49 +3,65 @@ package dataType
 import (
 	"database/sql"
 	"database/sql/driver"
+	"gorm.io/gorm"
+	"gorm.io/gorm/schema"
 	"strings"
 	"time"
 )
 
-type CustomTime struct {
-	time.Time
-}
+type CustomTime time.Time
 
 func (this *CustomTime) Scan(value interface{}) (err error) {
 	nullTime := &sql.NullTime{}
 	err = nullTime.Scan(value)
-	this.Time = nullTime.Time
+	*this = CustomTime(nullTime.Time)
 	return
 }
 
 func (this CustomTime) Value() (driver.Value, error) {
-	return this.Time, nil
+	return this, nil
 }
 
 func (this CustomTime) GormDataType() string {
 	return "time"
 }
 
+// GormDBDataType gorm db data type
+func (CustomTime) GormDBDataType(db *gorm.DB, field *schema.Field) string {
+	switch db.Dialector.Name() {
+	case "sqlite":
+		return "timestamp"
+	case "mysql":
+		return "timestamp"
+	case "postgres":
+		return "timestamp with time zone"
+	case "sqlserver":
+		return "timestamp"
+	}
+	return ""
+}
+
 func (this CustomTime) GobEncode() ([]byte, error) {
-	return this.Time.GobEncode()
+	return this.GobEncode()
 }
 
 func (this *CustomTime) GobDecode(b []byte) error {
-	return (this.Time).GobDecode(b)
+	return this.GobDecode(b)
 }
 
 func (this CustomTime) MarshalJSON() ([]byte, error) {
 	b := []byte{'"'}
-	b = append(b, []byte(this.Time.Format(time.DateTime))...)
+	b = append(b, []byte(time.Time(this).Format(time.DateTime))...)
 	b = append(b, '"')
 	return b, nil
 }
 func (this *CustomTime) UnmarshalJSON(b []byte) (err error) {
 	s := strings.Trim(string(b), "\"")
 	if s == "null" {
-		this.Time = time.Time{}
+		*this = CustomTime{}
 		return nil
 	}
-	this.Time, err = time.ParseInLocation(time.DateTime, s, time.FixedZone("CST", 8*3600))
+	_t, err := time.ParseInLocation(time.DateTime, s, time.FixedZone("CST", 8*3600))
+	*this = CustomTime(_t)
 	return err
 }
