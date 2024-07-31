1270ad5732d6e29ed2bf95f55a4f722c49b96bd8
helei <helei@itestor.com>
2024-07-31 09:43:23 +0800

    数据库 自定义类型，json通用类编码解码类函数。

---
+++ userDb/dbGormTools.go
@@ -0,0 +1,49 @@
+package userDb
+
+import (
+	"database/sql/driver"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"gorm.io/gorm"
+	"gorm.io/gorm/schema"
+)
+
+func GormDbDataType(db *gorm.DB, field *schema.Field) string {
+	switch db.Dialector.Name() {
+	case "sqlite":
+		return "JSON"
+	case "mysql":
+		return "longtext"
+	case "postgres":
+		return "JSONB"
+	case "sqlserver":
+		return "NVARCHAR(MAX)"
+	}
+	return ""
+}
+
+func GormDbDataValue(d any) (driver.Value, error) {
+	if d == nil {
+		return nil, nil
+	}
+	b, err := json.Marshal(d)
+	return string(b), err
+}
+
+func GormDbScan(val any, dst any) error {
+	if val == nil {
+		return nil
+	}
+	var ba []byte
+	switch v := val.(type) {
+	case []byte:
+		ba = v
+	case string:
+		ba = []byte(v)
+	default:
+		return errors.New(fmt.Sprint("Failed to unmarshal JSONB value:", val))
+	}
+	err := json.Unmarshal(ba, dst)
+	return err
+}
