87da7d516ce6ee4f50854cf28d62d387f9fbfd93
helei <helei@itestor.com>
2024-11-05 16:55:53 +0800

    新增 字符串格式化规则引擎，目前支持日期类的统一
    Signed-off-by: helei <helei@itestor.com>

--- formatRuleEngine/format.rule.go
+++ formatRuleEngine/format.rule.go
@@ -3,7 +3,7 @@ package formatRuleEngine
 type FormatRule struct {
 	FormatType string   `yaml:"format_type" json:"format_type"` // 格式化类型
 	InputRules []string `yaml:"input_rules" json:"input_rules"` // 识别格式化规则
-	OutputRule string   `yaml:"out_rule" json:"output_rule"`    //  输出格式规则
+	OutputRule string   `yaml:"output_rule" json:"output_rule"` //  输出格式规则
 }
 
 // Format 格式化
