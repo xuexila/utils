4ea5b9a204a7070416f961b4f6bdb92fba325fc6
helei <helei@itestor.com>
2024-11-15 10:50:48 +0800

    新增 map 反转函数

--- http/httpTools/http.tools.go
+++ http/httpTools/http.tools.go
@@ -1,7 +1,9 @@
 package httpTools
 
 import (
+	"fmt"
 	"net/http"
+	"net/url"
 	"strings"
 )
 
@@ -13,4 +15,12 @@ func QueryGetSlice(r *http.Request, key string, step string) []string {
 		return nil
 	}
 	return strings.Split(v, step)
+}
+
+// SetDisposition 文件下载时候，设置中文文件名
+func SetDisposition(w http.ResponseWriter, filename string) {
+	encodedFileName := url.QueryEscape(filename)
+	// 设置Content-Disposition头部，使用RFC5987兼容的方式指定文件名
+	contentDisposition := fmt.Sprintf("attachment; filename=\"%s\"; filename*=UTF-8''%s", encodedFileName, encodedFileName)
+	w.Header().Set("Content-Disposition", contentDisposition)
 }
