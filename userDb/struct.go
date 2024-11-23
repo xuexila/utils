3ddcca5b3d4935608846550ffb243274bf491a77
sunshy boy <ccdacom@yeah.net>
2024-11-24 01:43:52 +0800

    数据库 json对象

--- userDb/struct.go
+++ userDb/struct.go
@@ -9,7 +9,7 @@ type Dbbase struct {
 	Pwd          string               `ini:"pwd" yaml:"pwd" json:"pwd" gorm:"type:varchar(512);not null;comment:数据库密码"`
 	Encrypt      string               `ini:"encrypt" yaml:"encrypt" json:"encrypt" gorm:"type:varchar(64);not null;default:none;comment:密码加密存储方式，默认none明文"` // 是否加密
 	Dbname       string               `ini:"dbname" yaml:"dbname" json:"dbname" gorm:"type:varchar(128);not null;comment:默认连接的库"`
-	MaxIdleConns int                  `ini:"max_idle_conns" yaml:"maxIdleConns" json:"maxIdleConns" gorm:"type:int;not null;default:2;comment:最大空闲连接数"`
-	MaxOpenConns int                  `ini:"max_open_conns" yaml:"maxOpenConns" json:"maxOpenConns" gorm:"type:int;not null;default:10;comment:最大连接数"`
+	MaxIdleConns int                  `ini:"max_idle_conns" yaml:"max_idle_conns" json:"max_idle_conns" gorm:"type:int;not null;default:2;comment:最大空闲连接数"`
+	MaxOpenConns int                  `ini:"max_open_conns" yaml:"max_open_conns" json:"max_open_conns" gorm:"type:int;not null;default:10;comment:最大连接数"`
 	Remark       string               `ini:"remark" yaml:"remark" json:"remark" gorm:"type:varchar(256);not null;uniqueIndex;comment:备注信息，只能唯一"`
 }
