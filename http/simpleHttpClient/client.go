a89cd2270badc9a9e7dcd62f57de90281f6e8c0a
helei <helei@itestor.com>
2024-12-18 09:52:20 +0800

    http 简单 客户端优化
    Signed-off-by: helei <helei@itestor.com>

--- http/simpleHttpClient/client.go
+++ http/simpleHttpClient/client.go
@@ -11,13 +11,27 @@ import (
 	"time"
 )
 
+var simpleClient *http.Client
+
 // InitHttpClient 初始化http client
-func InitHttpClient(timeout time.Duration, args ...string) (http.Client, error) {
+func InitHttpClient(timeout time.Duration, args ...string) (*http.Client, error) {
+	if simpleClient != nil {
+		return simpleClient, nil
+	}
+
 	trans := &http.Transport{
-		DisableKeepAlives: true,
 		TLSClientConfig: &tls.Config{
 			InsecureSkipVerify: true, // client 不对https 证书进行校验
 		},
+		DisableKeepAlives:   false,           // 是否禁用 keep-alives
+		TLSHandshakeTimeout: 5 * time.Second, // tls 握手超时
+		DisableCompression:  false,           // 是否禁用压缩，默认情况下应保持 false 以允许接收压缩内容，除非有特定原因需要禁用。
+		// 连接池相关配置
+		MaxIdleConns:        1000, // 控制整个客户端的最大空闲连接数
+		MaxIdleConnsPerHost: 5,    // 限制每个主机的最大空闲连接数
+		MaxConnsPerHost:     0,    // 限制每个主机的最大连接数（包括活跃和空闲）
+		IdleConnTimeout:     0,    // 设置空闲连接在被关闭前等待新请求的时间
+
 	}
 	if len(args) >= 2 {
 		proxyAddr := args[1]
@@ -25,7 +39,7 @@ func InitHttpClient(timeout time.Duration, args ...string) (http.Client, error) 
 		case "socks5":
 			dialer, err := proxy.SOCKS5("tcp", proxyAddr, nil, proxy.Direct)
 			if err != nil {
-				return http.Client{}, fmt.Errorf("newHttpClient socks5 proxy error: %v", err)
+				return simpleClient, fmt.Errorf("newHttpClient socks5 proxy error: %v", err)
 			}
 			trans.DialContext = func(ctx context.Context, network, addr string) (net.Conn, error) {
 				return dialer.Dial(network, addr)
@@ -33,13 +47,14 @@ func InitHttpClient(timeout time.Duration, args ...string) (http.Client, error) 
 		case "http":
 			u, err := url.Parse(proxyAddr)
 			if err != nil {
-				return http.Client{}, fmt.Errorf("newHttpClient parse proxy url error: %v", err)
+				return simpleClient, fmt.Errorf("newHttpClient parse proxy url error: %v", err)
 			}
 			trans.Proxy = http.ProxyURL(u)
 		}
 	}
-	return http.Client{
+	simpleClient = &http.Client{
 		Transport: trans,
 		Timeout:   timeout,
-	}, nil
+	}
+	return simpleClient, nil
 }
