911fe68289c8263d37aec19852ff1751b573602a
helei <helei@itestor.com>
2025-02-18 19:16:14 +0800

    any 2 float 优化
    Signed-off-by: helei <helei@itestor.com>

---
+++ dataType/file.go
@@ -0,0 +1,47 @@
+package dataType
+
+import (
+	"database/sql/driver"
+	"errors"
+	"gorm.io/gorm"
+)
+
+type Binary []byte
+
+// Scan 实现了 sql.Scanner 接口，用于从数据库读取二进制数据到 BinaryFile 类型
+func (bf *Binary) Scan(value interface{}) error {
+	if value == nil {
+		*bf = nil
+		return nil
+	}
+	bytes, ok := value.([]byte)
+	if !ok {
+		return errors.New("type assertion to []byte failed")
+	}
+	*bf = bytes
+	return nil
+}
+
+// Value 实现了 driver.Valuer 接口，用于将 BinaryFile 类型的数据写入数据库
+func (bf Binary) Value() (driver.Value, error) {
+	return bf, nil
+}
+
+// GormDataType 返回GORM的数据类型名称
+func (Binary) GormDataType() string {
+	return "binary"
+}
+
+// GormDBDataType 返回数据库特定的数据类型名称
+func (Binary) GormDBDataType(db *gorm.DB) string {
+	switch db.Dialector.Name() {
+	case "mysql":
+		return "longblob"
+	case "postgres":
+		return "BYTEA"
+	case "sqlite":
+		return "BLOB"
+	default:
+		return "BLOB"
+	}
+}
