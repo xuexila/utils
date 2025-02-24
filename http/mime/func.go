5a330a51a9d3721bb01d4e1b957e6125c438c7d4
helei <helei@itestor.com>
2025-02-24 11:00:54 +0800

    Curd通用组件优化
    Signed-off-by: helei <helei@itestor.com>

--- http/mime/func.go
+++ http/mime/func.go
@@ -1,6 +1,7 @@
 package mime
 
 import (
+	"bytes"
 	"fmt"
 	"github.com/helays/utils/close/osClose"
 	"io"
@@ -32,4 +33,24 @@ func GetFileMimeType(file io.Reader) (string, error) {
 	// 检测MIME类型
 	contentType := http.DetectContentType(buffer[:n])
 	return contentType, nil
+}
+
+// GetMimeType 接受一个已打开的文件指针并返回文件的MIME类型。
+func GetMimeType(file io.Reader) (io.Reader, string, error) {
+	// 读取文件的前512个字节
+	buffer := make([]byte, 512)
+	n, err := file.Read(buffer)
+	if err != nil && err != io.EOF {
+		return nil, "", fmt.Errorf("读取文件时出错: %w", err)
+	}
+	// 检测MIME类型
+	contentType := http.DetectContentType(buffer[:n])
+
+	// 如果需要重新使用这个文件，可以将已读取的数据和原Reader组合起来
+	combinedReader := io.MultiReader(bytes.NewReader(buffer[:n]), file)
+
+	// 假设你需要返回这个 combinedReader 以供后续使用
+	// 这里仅作为演示，实际使用中可能需要调整函数签名来返回 combinedReader
+	// 此处简化处理，直接返回 mimeType 和 nil 错误
+	return combinedReader, contentType, nil
 }
